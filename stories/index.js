51ce3483b4c28b73fe5b5678988d08cb19f8b1cf
Conrad <conradwen@gmail.com>
2021-12-13 00:40:13 -0800

    confirm and status appointment components

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,8 +161,21 @@ storiesOf("Appointment", module)
   ))
   .add("Show", () => (
     <Show
-      interviewer={interviewers}
+      student="Lydia Miller-Jones"
+      interviewer={interviewers[0]}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => (
+    <Status
+      message="Deleting"
     />
   ))
