c1c66fce2ac1a7f73292e35b70311c0e663f03e2
Conrad <conradwen@gmail.com>
2021-12-13 01:47:44 -0800

    appointment component combined

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-import Header from "./Header"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time="12pm" />
-      {props.time ? `Appointment at ${props.time}` : "No Appointments"}
+      <Header time={props.time} />
+      {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        /> :
+        <Empty />}
     </article>
   );
 }
