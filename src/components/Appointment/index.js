46b77dea03eb6ca28528827fb7bf3f8c1455c52d
Conrad <conradwen@gmail.com>
2021-12-15 06:15:46 -0800

    apointment render logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useVisualMode from "hooks/useVisualMode";
 
 import Header from "./Header";
 import Show from "./Show";
@@ -7,16 +8,23 @@ import Empty from "./Empty";
 import "./styles.scss";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
       {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        /> :
-        <Empty />}
+        />
+      )}
     </article>
   );
 }
